63e6393b57d009b6d0de772ec35fb6f1a64ebec9
geon-2 <gunle0416@gmail.com>
2025-12-27 03:13:58 +0900

    config: Set header meta data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
-import KakaoCallback from "./pages/KakaoCallback.tsx";
 
 function App () {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/kakao/callback" element={<KakaoCallback />} />
             </Routes>
         </BrowserRouter>
     )
